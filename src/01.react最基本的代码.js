2d5108fac0f40c5164e042aa4603ac095511aa6a
juju <957644070@qq.com>
2018-10-02 16:42:30 +0800

    ''

--- src/01.react最基本的代码.js
+++ src/01.react最基本的代码.js
@@ -7,8 +7,8 @@
 // 1. 这两个导入时候，接收的成员名称，必须这么写
 import React from 'react' // 创建组件、虚拟DOM元素，生命周期
 import ReactDOM from 'react-dom' // 把创建好的 组件 和 虚拟DOM 放到页面上展示的
-
-
+//区间
+//#region
 // 2. 创建虚拟DOM元素
 // 参数1： 创建的元素的类型，字符串，表示元素的名称
 // 参数2： 是一个对象或 null, 表示 当前这个 DOM 元素的属性
@@ -30,7 +30,7 @@ const mydiv = React.createElement('div', null, '这是一个div元素', myh1)
 // 参数2： 指定页面上的DOM元素，当作容器 
 ReactDOM.render(mytest, document.getElementById('app'))
 // Target container is not a DOM element.  经过分析，猜测：第二个参数接收的应该是一个  DOM 元素而不是 选择器
-
+//#endregion
 
 
 /* const vm = new Vue({
